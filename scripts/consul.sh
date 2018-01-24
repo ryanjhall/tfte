ea6e7499804eca2fd1d8181c2681f6a4ea338635
Ryan Hall <rhall@theazure.cloud>
2018-01-24 22:04:36 +1100

    unzip amendments

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -8,10 +8,9 @@ HOST=$2
 #
  # INSTALL PREREQUISITES
 #
-sudo apt-get update
-sudo apt-get install unzip
+sudo apt install unzip
 sudo curl https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip -o /usr/local/bin/consul.zip
-sudo unzip /usr/local/bin/consul
+sudo unzip /usr/local/bin/consul -d /usr/local/bin
 sudo chmod +x /usr/local/bin consul
 sudo mkdir -p /opt/consul/data
 
